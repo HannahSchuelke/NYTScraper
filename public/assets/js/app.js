151f63c342597854e46cb662948063a4b6b70958
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-21 22:45:05 -0500

    got /articles to populate and /scrape

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,14 +3,16 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#Article").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      // $("#articles").append("p.title" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $(Article).append("<div id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</div>");
+      // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
   });
   
-  // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
+  // Whenever someone clicks a div tag
+  $(document).on("click", "div", function() {
     // Empty the notes from the note section
-    $("#notes").empty();
+    // $("#notes").empty();
     // Save the id from the p tag
     var thisId = $(this).attr("data-id");
   
@@ -28,13 +30,13 @@ $.getJSON("/articles", function(data) {
       .then(function(data) {
         console.log(data);
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<div>" + data.title + "</div>");
         // An input to enter a new title
         $("#notes").append("<input id='titleinput' name='title' >");
         // A textarea to add a new note body
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        $("#notes").append("<button>'" + data._id + "</button>");
   
         // If there's a note in the article
         if (data.note) {
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //DEPENDANCIES
-var cheerio = require("cheerio"); 
-var axios = require("axios"); 
+var cheerio = require("cheerio");
+var axios = require("axios");
 var express = require("express");
 var morgan = require("morgan");
 var mongoose = require("mongoose");
 var expressHand = require("express-handlebars");
-var path = require ("path");
+var path = require("path");
 // const dotenv = require('dotenv'); //!!!!
 require('dotenv').config();
 // Requiring Article model
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Express view engine (don't know if I need this)
-app.engine('handlebars', expressHand({defaultLayout: 'main'}));
+app.engine('handlebars', expressHand({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
@@ -46,90 +46,101 @@ mongoose.connect("mongodb://localhost/redditdb", { useNewUrlParser: true });
 
 
 // /ROUTE (./ because we're in the same directory)
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/layouts/index.html');
-  
-}); 
+
+});
 
 // ROUTES TO SCRAPE
-app.get("/scrape", function(req, res) {
-      // An empty array to save the data that we'll scrape (now within scope)
-      var results = [];
-    // First, we grab the body of the html with axios
-    axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-      // With cheerio, find each p-tag with the "title" class
-      $("p.title").each(function(i, element) {
-        // Save the text of the element in a "title" variable
-        var title = $(element).text();
-        // In the currently selected element, look at its child elements (i.e., its a-tags),
-        // then save the values for any "href" attributes that the child elements may have
-        var link = $(element).children().attr("href");
-        // Save these results in an object that we'll push into the results array we defined earlier
-        results.push({
-          title: title,
-          link: link
-        });
+app.get("/scrape", function (req, res) {
+  // An empty array to save the data that we'll scrape (moved up to be within scope)
+  var results = [];
+  // First, we grab the body of the html with axios
+  axios.get("https://old.reddit.com/r/webdev/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+    // With cheerio, find each p-tag with the "title" class
+    $("p.title").each(function (i, element) {
+      // Save the text of the element in a "title" variable
+      var title = $(element).text();
+      // In the currently selected element, look at its child elements (i.e., its a-tags),
+      // then save the values for any "href" attributes that the child elements may have
+      var link = $(element).children().attr("href");
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+        title: title,
+        link: link
       });
-      // Log the results once you've looped through each of the elements found with cheerio
-      console.log(results); //go to my node terminal server
-    //   res.send(results);
-    
     });
-  //CREATE NEW ARTICLE
-    // Create a new Article using the `result` object built from scraping
-        db.Article.create(res)
-          .then(function(redditdb) {
-            // View the added result in the console
-            console.log(redditdb);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-    // Send a message to the client
-      // res.send("Scrape Complete");
-      res.json(results);
-      });
-   
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results); //go to my node terminal server
+    //   res.send(results);
 
-  
-  // ROUTE FOR GETTING ALL ARTICLES FROM THE DB
-  app.get("/articles", function(req, res) {
-    // TODO: Finish the route so it grabs all of the articles
-    db.Article.find({})
-    .then(Article => res.json(Article))
   });
-  
-  // Route for grabbing a specific Article by id, populate it with its note
-  app.get("/articles/:id", function(req, res) {
-    // TODO
-    // ====
-    // Finish the route so it finds one article using the req.params.id,
-    // and run the populate method with "note",
-    // then responds with the article with the note included
-    Article.findOne({_id: req.params.id})
+  //CREATE NEW ARTICLE
+  // Create a new Article using the `result` object built from scraping
+  db.Article.create(res)
+    .then(function (redditdb) {
+      //         // View the added result in the console
+      //         console.log(redditdb);
+      //       })
+      //       .catch(function(err) {
+      //         // If an error occurred, log it
+      //         console.log(err);
+      //       });
+      // // Send a message to the client
+      //   res.json(results);
+      //   });
+      res.json(redditdb);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+
+
+// ROUTE FOR GETTING ALL ARTICLES FROM THE DB
+app.get("/articles", function (req, res) {
+  // TODO: Finish the route so it grabs all of the articles
+  db.Article.find({})
+    .then(Article => res.json(Article))
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with its note
+app.get("/articles/:id", function (req, res) {
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "note",
+  // then responds with the article with the note included
+  db.Article.findOne({ _id: req.params.id })
     .populate("note")
-    .then( Article => res.json(Article))
-  }); 
-  
-  // Route for saving/updating an Article's associated Note
-  app.post("/articles/:id", function(req, res) {
-    // TODO
-    // ====
-    // save the new note that gets posted to the Notes collection
-    // then find an article from the req.params.id
-    // and update it's "note" property with the _id of the new note
-    Note.create(req.body)
-    .then( dbNote => db.Article.findOneAndUpdate(
-    {_id:req.params.id},
-    {$set:{note:dbNote.id}}
+    .then(Article => res.json(Article))
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  // TODO
+  // ====
+  // save the new note that gets posted to the Notes collection
+  // then find an article from the req.params.id
+  // and update it's "note" property with the _id of the new note
+  Note.create(req.body)
+    .then(dbNote => db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { note: dbNote.id } }
     ))
     .then(redditdb => res.json(redditdb))
-  });
+    // .catch(function (err) {
+    //   res.json(err);
+    // });
+});
 
 // Listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
